10757807c214bcabdbb5b1f1705cc047c20152ca
Kerem Üzer <keremuzer33@gmail.com>
2023-04-26 10:59:27 +0300

    add operate function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator){
+  if (operator === "+") add(num1, num2);
+  else if (operator === "-") substract(num1, num2);
+  else if (operator === "*") multiply(num1, num2);
+  else if (operator === "/") divide(num1, num2);
+}
+
 function add(x, y){
   return x+ y;
 }
