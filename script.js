3606ee93d4c2bd781e262e8775b1bc25df62e291
Kerem Üzer <keremuzer33@gmail.com>
2023-04-27 15:45:39 +0300

    bug fixes

--- script.js
+++ script.js
@@ -6,26 +6,30 @@ let operators = document.querySelectorAll(".operator");
 let display = document.querySelector(".display");
 let equal = document.getElementById("equal");
 let num = "";
+let result = 0;
+let ac = document.getElementById("ac");
 
 equal.addEventListener("click", function(){
   nums.push(parseInt(num));
   num = "";
 });
 
-equal.addEventListener("click", operate);
+equal.addEventListener("click", function() {
+  operate();
+  num = display.innerHTML;
+  nums = [];
+});
 
 function operate() {
   for(let i = 0; i < operations.length; i++){
     if(operations[i] === "x"){
       multiply(i);
       operations.splice(i, 1);
-      console.log(operations);
       operate();
     }
-    if(operations[i] === "/"){
+    if(operations[i] === "÷"){
       divide(i);
       operations.splice(i, 1);
-      console.log(operations);
       operate();
     }
   }
@@ -43,6 +47,8 @@ function operate() {
       operate();
     }
   }
+  display.innerHTML = result;
+  console.log(result);
 }
 
 numbers.forEach(number => {
@@ -52,43 +58,45 @@ numbers.forEach(number => {
   });
   number.addEventListener("click", function(){
     num += number.innerHTML;
-    console.log(num);
   });
 });
 
 operators.forEach(operator => {
   operator.addEventListener("click", function(){
     let current = display.innerHTML;
-    display.innerHTML = `${current} ${operator.innerHTML}`;
+    display.innerHTML = `${current} ${operator.id} `;
   })
   operator.addEventListener("click", function(){
-    nums.push(parseInt(num));
+    nums.push(parseFloat(num));
     operations.push(operator.id);
-    console.log(operations);
     num = "";
   });
 });
 
+ac.addEventListener("click", function() {
+  display.innerHTML = "";
+  nums = [];
+  num = "";
+  operations = [];
+  result = 0;
+});
+
 function add(j){
-  console.log(nums[j] + nums[j + 1]);
+  result = (nums[j] + nums[j + 1]);
   nums.splice(j, 2, nums[j] + nums[j + 1]);
-  console.log(nums);
 }
 
 function substract(j){
-  console.log(nums[j] - nums[j + 1]);
+  result = (nums[j] - nums[j + 1]);
   nums.splice(j, 2, nums[j] - nums[j + 1]);
-  console.log(nums);
 }
 
 function multiply(i){
-  console.log(nums[i] * nums[i + 1]);
+  result = (nums[i] * nums[i + 1]);
   nums.splice(i, 2, nums[i] * nums[i + 1]);
-  console.log(nums);
 }
 
 function divide(i){
-  console.log(nums[i] / nums[i + 1]);
+  result = (nums[i] / nums[i + 1]);
   nums.splice(i, 2, nums[i] / nums[i + 1]);
-  console.log(nums);
 }
